2e01023bf3a134a1da4c16672d2aeb92d682c7af
Sergey Klyusov <donot@email.me>
2024-08-21 20:02:34 +0500

    backing up

--- ipdb_static.go
+++ ipdb_static.go
@@ -205,7 +205,7 @@ func main() {
       e("div").R(t("Generated: " + now)),
       e("div").R(
         e("label", "class", "button", "onclick", "expandAll();").R(t("Разверуть все")),
-        e("label", "class", "button", "onclick", "collapseAll();").R(t("Сверуть все")),
+        e("label", "class", "button", "onclick", "collapseAll();").R(t("Свернуть все")),
       ),
       e("H1").R(t(`Сети IPv4`)),
       func() (_ any) {
